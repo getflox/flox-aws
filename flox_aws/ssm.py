c144879333471d5c8a05810f2261a11ef204fac6
Michal Przytulski <michal@przytulski.pl>
2018-07-08 23:00:14 +0100

    list and remove parameteres

--- flox_aws/ssm.py
+++ flox_aws/ssm.py
@@ -1,5 +1,7 @@
 import boto3
 import click
+from click import Abort
+from humanfriendly.tables import format_smart_table
 
 from flox_aws.kms import KMS
 
@@ -34,6 +36,51 @@ class SSM(object):
 
         return name
 
+    def get_parameters(self, namespace):
+        parameters = {}
+        search_params = dict(Path=namespace, WithDecryption=True, Recursive=True)
+
+        while search_params.get('NextToken', 0) is not None:
+            results = self.ssm.get_parameters_by_path(**search_params)
+
+            parameters.update(
+                dict(map(lambda x: (x['Name'], x['Value']), results.get('Parameters')))
+            )
+
+            search_params['NextToken'] = results.get('NextToken', None)
+
+        return parameters
+
+    def describe_parameters(self, namespace):
+        search_params = dict(
+            ParameterFilters=[dict(Key='Name', Option='BeginsWith', Values=[namespace])],
+            MaxResults=50
+        )
+        parameters = {}
+
+        while search_params.get('NextToken', 0) is not None:
+            results = self.ssm.describe_parameters(**search_params)
+
+            parameters.update(
+                dict(map(lambda x: (x['Name'], x), results.get('Parameters')))
+            )
+
+            search_params['NextToken'] = results.get('NextToken', None)
+
+        values = self.get_parameters(namespace)
+
+        for name, param in parameters.items():
+            yield [
+                name,
+                values.get(name, {}),
+                param.get('Type'),
+                param.get('LastModifiedDate'),
+                param.get('LastModifiedUser'),
+            ]
+
+    def remove(self, name):
+        return self.ssm.delete_parameter(Name=name)
+
 
 @click.group()
 @click.pass_obj
@@ -61,11 +108,31 @@ def write(flox, namespace, encrypt, name, value):
 
 
 @ssm.command(name="list")
-def list_parameters():
+@click.option('--namespace', help="Namespace under which parameter should be stored, for example service:name")
+@click.pass_obj
+def list_parameters(flox, namespace):
     """List all service parameters for given namespace"""
+    namespace = namespace or f'/service/{flox.name}'
 
+    click.secho(
+        format_smart_table(
+            flox.ssm.describe_parameters(namespace),
+            ['Name', 'Value', 'Type', 'Modified', 'User']
+        )
+    )
 
 
 @ssm.command()
-def remove():
+@click.option('--namespace', help="Namespace under which parameter should be stored, for example service:name")
+@click.argument('name')
+@click.pass_obj
+def remove(flox, namespace, name):
     """Remove named parameter"""
+    namespace = namespace or f'/service/{flox.name}'
+    key = f'{namespace}/{name}'
+
+    if not click.confirm(f'Delete "{key}"?'):
+        raise Abort
+
+    flox.ssm.remove(key)
+    click.secho('Done.', fg='green')
