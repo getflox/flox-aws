db0abaaf5fdcfb7c1ffad0fdbb862ff6ea7e97e5
Michal Przytulski <michal@przytulski.pl>
2018-07-08 15:40:50 +0100

    ssm and kms

---
+++ flox_aws/kms.py
@@ -0,0 +1,67 @@
+import base64
+
+
+class KMSException(Exception):
+    pass
+
+
+class KMS(object):
+    def __init__(self, kms, key='parameter_store_key'):
+        self.key = key
+        self.kms = kms
+
+    def get_key_by_alias(self, alias=None):
+        """
+        Return KMS KeyId for give alias
+        :param alias:
+        :return:
+        """
+        aliases = self.kms.list_aliases()
+
+        key_id = (next(iter(filter(
+            lambda x: x.get('AliasName') == 'alias/' + alias,
+            aliases.get('Aliases', {})
+        ))) or {}).get('TargetKeyId')
+
+        if key_id:
+            return key_id
+
+        raise KMSException('Unable to locate KMS key with "{}" alias.'.format(alias))
+
+    def list_aliases(self, aws=False):
+        """
+        :param aws: Include AWS build-in aliases
+        :return:
+        """
+        return filter(
+            lambda x: (x.get('AliasName').startswith('alias/aws') and aws) or not x.get('AliasName').startswith('alias/aws'),
+            self.kms.list_aliases().get('Aliases')
+        )
+
+    def encrypt(self, value, key=None):
+        """
+        Encrypt plain text value using KMS service and default or named key
+        :param value: Plain text value
+        :param key: Optionally, encryption key to be used
+        :return:
+        """
+        key_id = self.get_key_by_alias(key or self.key)
+        response = self.kms.encrypt(
+            KeyId=key_id,
+            Plaintext=bytes(value),
+        )
+
+        return base64.b64encode(response.get("CiphertextBlob")), key_id
+
+    def decrypt(self, encrypted):
+        """
+        Decyrpt given value using AWS KMS service
+        :param encrypted: base64 encoded encrypted value
+        :return: Plain text decrypted value
+        """
+        try:
+            return self.kms.decrypt(
+                CiphertextBlob=base64.b64decode(encrypted)
+            ).get('Plaintext').decode("utf-8")
+        except Exception as e:
+            raise KMSException('Failed to decrypt given value')
--- flox_aws/ssm.py
+++ flox_aws/ssm.py
@@ -1,8 +1,71 @@
+import boto3
 import click
+
+from flox_aws.kms import KMS
+
+
+class SSM(object):
+    def __init__(self, ssm, kms, key='parameter_store_key'):
+        self.ssm = ssm
+        self.kms = kms
+        self.key = key
+
+    def write(self, name, value, encrypt=True, tags=None):
+        args = {
+            'Name': name,
+            'Value': value,
+            'Type': 'SecureString' if encrypt else 'String',
+            'Overwrite': True
+        }
+
+        if encrypt:
+            args['KeyId'] = self.kms.get_key_by_alias(self.key)
+
+        self.ssm.put_parameter(**args)
+
+        if not tags:
+            return name
+
+        self.ssm.add_tags_to_resource(
+            ResourceType='Parameter',
+            ResourceId=name,
+            Tags=[{'Key': k, 'Value': v} for k, v in tags.items()]
+        )
+
+        return name
 
 
 @click.group()
-def cli():
-    """SubCommandline interface for yourpackage."""
-    click.echo("test")
+@click.pass_obj
+def ssm(flox):
+    """Manage SSM parameters"""
+    flox.ssm = SSM(boto3.client("ssm"), KMS(boto3.client("kms")))
 
+
+@ssm.command()
+@click.option('--namespace', help="Namespace under which parameter should be stored, for example service:name")
+@click.option('--encrypt/--no-encrypt', help="Encrypt value", default=True)
+@click.argument('name')
+@click.argument('value', required=False)
+@click.pass_obj
+def write(flox, namespace, encrypt, name, value):
+    """Write parameter to SSM storage"""
+    namespace = namespace or f'/service/{flox.name}'
+    key = f'{namespace}/{name}'
+
+    value = value or click.open_file('-', 'r').read()
+
+    flox.ssm.write(key, value, encrypt)
+
+    click.secho(f'Value: "{value}" has been stored under: "{key}"')
+
+
+@ssm.command(name="list")
+def list_parameters():
+    """List all service parameters for given namespace"""
+
+
+
+@ssm.command()
+def remove():
+    """Remove named parameter"""
